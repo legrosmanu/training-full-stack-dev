e512d64a04dc588584578dd608db208ae98d90ed
Emmanuel Legros <emmanuel.legros@gmail.com>
2020-12-04 16:10:05 +0100

    Assignement 3: user authent

--- node-js/conFusionServer/routes/users.js
+++ node-js/conFusionServer/routes/users.js
@@ -8,8 +8,12 @@ var authenticate = require('../authenticate');
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+  User.find().then((users) => {
+    res.status = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json(users);
+  }).catch((err) => next(err));
 });
 
 router.post('/signup', (req, res, next) => {

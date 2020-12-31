7b47f858e6cbcbe14f994c24429b47026effff0c
Emmanuel Legros <emmanuel.legros@gmail.com>
2020-12-31 15:21:27 +0100

    React Animations

--- react/confusion/src/components/MainComponent.js
+++ react/confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Home from './HomeComponent';
 import About from './AboutComponent';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
-
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -73,14 +73,18 @@ class Main extends Component {
         return (
             <React.Fragment>
                 <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
-                    <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-                    <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-                    <Redirect to="/home" />
-                </Switch>
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route path='/home' component={HomePage} />
+                            <Route path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
+                            <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                            <Route path='/menu/:dishId' component={DishWithId} />
+                            <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                            <Redirect to="/home" />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
                 <Footer />
             </React.Fragment>
         );

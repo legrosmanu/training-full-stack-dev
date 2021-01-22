ae0e563febb287a51e92302701abb1e9c8e4bc9f
Emmanuel Legros <emmanuel.legros@gmail.com>
2021-01-22 14:26:50 +0100

    Add auth fake behavior

--- angular/src/app/auth/auth.component.ts
+++ angular/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  authStatus: boolean;
+
+  constructor(private authService: AuthService) {
+    this.authStatus = false;
+  }
 
   ngOnInit(): void {
+    this.authStatus = this.authService.isAuth;
   }
+
+  onSignIn(): void {
+    this.authService.signIn().then(() => {
+      this.authStatus = this.authService.isAuth;
+    });
+  }
+
+  onSignOut(): void {
+    this.authService.signOut();
+    this.authStatus = this.authService.isAuth;
+  }
+
 
 }

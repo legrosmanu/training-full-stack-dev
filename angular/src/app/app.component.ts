549532652fa3eacb071afcb1078f373eedbf00e0
Emmanuel Legros <emmanuel.legros@gmail.com>
2021-01-26 14:38:31 +0100

    Simple and infinite observable

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  secondes: number;
+
+  constructor() {
+    this.secondes = 0;
+  }
+
+  ngOnInit(): void {
+    const counter = interval(1000);
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+  }
 
 }

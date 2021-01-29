d062063c4bf46beb16846ff7dc8f04f85e1a6b11
Emmanuel Legros <emmanuel.legros@gmail.com>
2021-01-29 13:37:06 +0100

    Add the routing

--- angular-ex2/src/app/app-routing.module.ts
+++ angular-ex2/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'books', component: BookListComponent },
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'books/view/:id', component: SingleBookComponent },
+  { path: '', component: BookListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

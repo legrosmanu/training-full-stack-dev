a5423598f2ed9e45549994a77b1f5d1a717943dd
Emmanuel Legros <emmanuel.legros@gmail.com>
2021-01-29 16:05:24 +0100

    Linked to firebase

--- angular-ex2/src/app/app.component.ts
+++ angular-ex2/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
+import * as configEx2 from './config.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ex2';
+  constructor() {
+    // Initialize Firebase
+    firebase.initializeApp(configEx2.firebase);
+  }
 }

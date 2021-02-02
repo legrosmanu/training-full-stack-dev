41492004ae515fcaf6a66b70686d96a5ac064e1b
Emmanuel Legros <emmanuel.legros@gmail.com>
2021-02-02 11:34:53 +0100

    Add a service to manage the list of posts

--- angular-ex1/src/app/post-list-component/post-list-component.component.ts
+++ angular-ex1/src/app/post-list-component/post-list-component.component.ts
@@ -1,38 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { BlogPost } from '../models/BlogPost';
+import { BlogPostServiceService } from '../services/blog-post-service.service';
 
 @Component({
   selector: 'app-post-list-component',
   templateUrl: './post-list-component.component.html',
   styleUrls: ['./post-list-component.component.css']
 })
-export class PostListComponentComponent implements OnInit {
+export class PostListComponentComponent implements OnInit, OnDestroy {
 
   posts: Array<BlogPost>;
+  postsServiceSubscription?: Subscription;
 
-  constructor() {
+  constructor(private blogPostService: BlogPostServiceService) {
     this.posts = [];
-    let newPost = new BlogPost();
-    newPost.title = "Mon premier post";
-    newPost.content = "Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.";
-    this.posts.push(newPost);
-    newPost = new BlogPost();
-    newPost.title = "Mon second post";
-    newPost.content = "Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.";
-    this.posts.push(newPost);
-    newPost = new BlogPost();
-    newPost.title = "Et encore un post";
-    newPost.content = `Ut enim benefici liberalesque sumus, non ut exigamus gratiam (neque enim beneficium faeneramur sed natura propensi ad liberalitatem sumus), sic amicitiam non spe mercedis adducti sed quod omnis eius fructus in ipso amore inest, expetendam putamus.
-    Et quoniam apud eos ut in capite mundi morborum acerbitates celsius dominantur, ad quos vel sedandos omnis professio medendi torpescit, excogitatum est adminiculum sospitale nequi amicum perferentem similia videat, additumque est cautionibus paucis remedium aliud satis validum, ut famulos percontatum missos quem ad modum valeant noti hac aegritudine colligati, non ante recipiant domum quam lavacro purgaverint corpus. ita etiam alienis oculis visa metuitur labes.
-    Et quoniam apud eos ut in capite mundi morborum acerbitates celsius dominantur, ad quos vel sedandos omnis professio medendi torpescit, excogitatum est adminiculum sospitale nequi amicum perferentem similia videat, additumque est cautionibus paucis remedium aliud satis validum, ut famulos percontatum missos quem ad modum valeant noti hac aegritudine colligati, non ante recipiant domum quam lavacro purgaverint corpus. ita etiam alienis oculis visa metuitur labes.`;
-    this.posts.push(newPost);
-    newPost = new BlogPost();
-    newPost.title = "Petit post";
-    newPost.content = "Pouet.";
-    this.posts.push(newPost);
   }
 
   ngOnInit(): void {
+    this.postsServiceSubscription = this.blogPostService.postsSubject.subscribe((posts:Array<BlogPost>) => {
+      this.posts = posts;
+    });
+    this.blogPostService.emitPosts();
+  }
+
+  ngOnDestroy(): void {
+    this.blogPostService.postsSubject.unsubscribe();
   }
 
 }
---
+++ angular-ex1/src/app/services/blog-post-service.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { BlogPost } from '../models/BlogPost';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlogPostServiceService {
+
+  posts: Array<BlogPost>;
+  postsSubject: Subject<BlogPost[]>;
+
+  constructor() {
+    this.posts = [];
+    this.postsSubject = new Subject<BlogPost[]>();
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    let newPost = new BlogPost();
+    newPost.title = "Mon premier post";
+    newPost.content = "Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.";
+    this.posts.push(newPost);
+    newPost = new BlogPost();
+    newPost.title = "Mon second post";
+    newPost.content = "Itaque tum Scaevola cum in eam ipsam mentionem incidisset, exposuit nobis sermonem Laeli de amicitia habitum ab illo secum et cum altero genero, C. Fannio Marci filio, paucis diebus post mortem Africani. Eius disputationis sententias memoriae mandavi, quas hoc libro exposui arbitratu meo; quasi enim ipsos induxi loquentes, ne 'inquam' et 'inquit' saepius interponeretur, atque ut tamquam a praesentibus coram haberi sermo videretur.";
+    this.posts.push(newPost);
+    newPost = new BlogPost();
+    newPost.title = "Et encore un post";
+    newPost.content = `Ut enim benefici liberalesque sumus, non ut exigamus gratiam (neque enim beneficium faeneramur sed natura propensi ad liberalitatem sumus), sic amicitiam non spe mercedis adducti sed quod omnis eius fructus in ipso amore inest, expetendam putamus.
+    Et quoniam apud eos ut in capite mundi morborum acerbitates celsius dominantur, ad quos vel sedandos omnis professio medendi torpescit, excogitatum est adminiculum sospitale nequi amicum perferentem similia videat, additumque est cautionibus paucis remedium aliud satis validum, ut famulos percontatum missos quem ad modum valeant noti hac aegritudine colligati, non ante recipiant domum quam lavacro purgaverint corpus. ita etiam alienis oculis visa metuitur labes.
+    Et quoniam apud eos ut in capite mundi morborum acerbitates celsius dominantur, ad quos vel sedandos omnis professio medendi torpescit, excogitatum est adminiculum sospitale nequi amicum perferentem similia videat, additumque est cautionibus paucis remedium aliud satis validum, ut famulos percontatum missos quem ad modum valeant noti hac aegritudine colligati, non ante recipiant domum quam lavacro purgaverint corpus. ita etiam alienis oculis visa metuitur labes.`;
+    this.posts.push(newPost);
+    newPost = new BlogPost();
+    newPost.title = "Petit post";
+    newPost.content = "Pouet.";
+    this.posts.push(newPost);
+    this.emitPosts();
+  }
+
+  emitPosts(): void {
+    this.postsSubject.next(this.posts.slice());
+  }
+
+}
